ee25e50fcdd94ba51c8620b16bc1ca8031d06971
Ashish Rai <99983844+MythicPlayer1@users.noreply.github.com>
2023-03-08 13:16:51 +0545

    Add files via upload

--- Complete Frontend/src/pages/LoginPage.js
+++ Complete Frontend/src/pages/LoginPage.js
@@ -1,97 +1,154 @@
-import { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { UserContext } from "../components/UserContext";
 
-import "./LoginPage.css";
+
+import { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { UserContext} from '../components/UserContext';
+
+
+
+import './LoginPage.css';
 const LoginPage = () => {
+
     const navigate = useNavigate();
 
-    const { User, setUser } = useContext(UserContext);
-    const [newUsername, setUsername] = useState();
-    const [newPassword, setPassword] = useState();
+    const { User, setUser, } = useContext(UserContext)
+    const [newUsername, setUsername] = useState('');
+    const [newPassword, setPassword] = useState('');
+
+    const getApi=async()=>{
+        const response= await fetch('http://localhost:3500/api_login',{
+            method:"POST",
+            headers:{
+                "Content-Type":'application/json',
+            },
+            body:JSON.stringify({
+                newUsername,
+                newPassword
+            })
+        })
+        const result= await response.json();
+        if(result.user){
+            alert("login sucessful")
+            // window.location.href='/dashboard'
+            navigate('/dashboard')
+
+        }else{
+            alert('Login unsuccessful, Try again !!')
+        }
+    }
+
+    const getAdminApi=async()=>{
+        const response= await fetch('http://localhost:3500/api_login',{
+            method:"POST",
+            headers:{
+                "Content-Type":'application/json',
+            },
+            body:JSON.stringify({
+                newUsername,
+                newPassword
+            })
+        })
+        const result= await response.json();
+        if(result.user){
+            alert("login sucessful")
+            // window.location.href='/dashboard'
+            navigate('/Admin')
+
+        }else{
+            alert('Login unsuccessful, Try again !!')
+        }
+    }
+    
+
 
     const takevalue = () => {
-        return {
-            username: newUsername,
-            password: newPassword,
-        };
-    };
+        return (
+            {
+                username: newUsername,
+                password: newPassword
+
+            }
+        )
+
+    }
 
     const titilechanger = (event) => {
-        setUsername(event.target.value);
-    };
+        setUsername(event.target.value)
+    }
 
     const passwordchanger = (event) => {
-        setPassword(event.target.value);
-    };
+        setPassword(event.target.value)
+    }
+
 
     const submithandler = async (event) => {
-        event.preventDefault();
+
+        event.preventDefault()
         const Datas = takevalue();
-        setUser(Datas);
-        setUser(true);
+        setUser(Datas)
+        setUser(true)
+        getApi();
         console.log(Datas);
-        setPassword("");
-        setUsername("");
-        navigate("/");
-    };
+        //navigate('/dashboard')
+        setPassword('')
+        setUsername("")
+        
+
+    }
+    const adminsubmithandler = async (event) => {
+
+        event.preventDefault()
+        const Datas = takevalue();
+        setUser(Datas)
+        setUser(true);
+        getAdminApi();
+        console.log(Datas);
+        // navigate('/admin')
+        setPassword('')
+        setUsername("")
+        
+
+    }
+    console.log(User)
+   
+
 
     return (
         <div>
-            <div className="lomain-container">
-                <div className="losubmain-container">
-                    <img src="img/cologo.png"></img>
+            <div className='lomain-container'>
+                <div className='losubmain-container'>
+                    <img src='images/logo.png'></img>
                 </div>
-                <h1
-                    style={{
-                        fontSize: "4vw",
-                        color: "white",
-                        fontWeight: "700",
-                        marginTop: "100px",
-                    }}
-                >
-                    Login
-                </h1>
-                <p
-                    style={{
-                        color: "white",
-                        fontSize: "20px",
-                        wordSpacing: "2px",
-                        padding: "7px",
-                    }}
-                >
-                    Sign in here to get started.
-                </p>
+                <h1 style={{fontSize:"4vw", color:"white",fontWeight:"700", marginTop:"100px"}}>Login</h1>
+                <p style={{color:"white",fontSize:"20px", wordSpacing:"2px", padding:"7px"}}>Sign in here to get started.</p>
 
                 <div className="lo-main">
-                    <form className="form_feild" onSubmit={submithandler}>
-                        <div className="text_feild">
-                            <label>Username</label>
+                    <form className='form_feild' onSubmit={submithandler}>
+                        <div className='text_feild'>
+                            <label >Username</label>
                             <span></span>
-                            <input
-                                type="text"
-                                onChange={titilechanger}
-                                value={newUsername}
-                            ></input>
+                            <input type="text" onChange={titilechanger} value={newUsername}></input>
+
                         </div>
-                        <div className="text_feild">
+                        <div className='text_feild'>
                             <label>Password</label>
                             <span></span>
-                            <input
-                                type="password"
-                                onChange={passwordchanger}
-                                value={newPassword}
-                            />
+                            <input type="password" onChange={passwordchanger} value={newPassword} />
                         </div>
-                        <div className="pass">Forget The Password?</div>
-                        <button className="lo-sub" type="submit">
-                            Sign In
-                        </button>
+                        <div className='pass'></div>
+                         <button className="lo-sub" onClick={submithandler} type='submit'>Sign In</button>
+                         <button className="lo-sub"  onClick={adminsubmithandler} type='submit'>Sign in as Admin</button>
+
                     </form>
+
                 </div>
+                <p style={{color:"white", marginTop:"400px"}}>Don't have an account? <Link to={'/registration'}>Create an account</Link></p>
             </div>
         </div>
-    );
-};
+
+
+    )
+}
 
 export default LoginPage;
--- Complete Frontend/src/pages/RegistrationPage.js
+++ Complete Frontend/src/pages/RegistrationPage.js
@@ -8,37 +8,14 @@ import { schema } from '../components/validationSchema';
 
 
 const initialValues = {
-    fullname: "",
-    username: "",
-    email: "",
-    password: "",
-    phone: "",
-};
+    fullname: '',
+    username: '',
+    email: '',
+    password: '',
+    phone: '',
+}
 
 export const RegistrationPage = () => {
-    const navigate = useNavigate();
-    let array = ""; // You cannot assign values to const variables once it is declared. So dont use const ....
-    const { errors, handleBlur, handleChange, handleSubmit, values } =
-        useFormik({
-            initialValues: initialValues,
-            validationSchema: schema,
-            onSubmit: () => {
-                array = {
-                    ...values,
-                    gender: gender1,
-                };
-                console.log(array);
-
-                navigate("/login");
-                // console.log(array)
-            },
-        });
-    const [gender1, setgender] = useState();
-
-    const clickhandler = (event) => {
-        setgender(event.target.value);
-    };
-    console.log(errors);
     const [gender1, setgender] = useState("")
     const [userDetail, setuserDetail] = useState("")
     const navigate = useNavigate()
@@ -98,179 +75,63 @@ export const RegistrationPage = () => {
 
     return (
         <div>
-            <div className="lomain-container">
-                <div className="losubmain-container">
-                    <img src="img/cologo.png" alt="NA" />
             <div className='lomain-container'>
                 <div className='losubmain-container'>
                     <img src='images/logo.png'></img>
                 </div>
-                <h1
-                    style={{
-                        fontSize: "4vw",
-                        color: "white",
-                        fontWeight: "700",
-                        marginTop: "30px",
-                    }}
-                >
-                    Registration Form
-                </h1>
+                <h1 style={{ fontSize: "4vw", color: "white", fontWeight: "700", marginTop: "30px" }}>Registration Form</h1>
                 {/* <p style={{ color: "white", fontSize: "20px", wordSpacing: "2px", padding: "7px" }}>Sign up here to get started.</p> */}
 
                 <div className="reg-main">
-                    <form className="form_feild" onSubmit={handleSubmit}>
-                        <div className="text_feild">
-                            <label>Fullname</label>
+                    <form className='form_feild' onSubmit={handleSubmit} >
+                        <div className='text_feild'>
+                            <label >Fullname</label>
                             <span></span>
-                            <input
-                                type="text"
-                                name="fullname"
-                                value={values.fullname}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            ></input>
-                            {
-                                <p
-                                    style={{
-                                        fontSize: "10px",
-                                        marginBottom: "2px",
-                                        color: "#961d1d",
-                                    }}
-                                >
-                                    {errors.fullname}
-                                </p>
-                            }
+                            <input type="text" name='fullname' value={values.fullname} onChange={handleChange} onBlur={handleBlur}></input>
+                            {<p style={{ fontSize: '10px', marginBottom: '2px', color: '#961d1d' }}>{errors.fullname}</p>}
+
                         </div>
-                        <div className="text_feild">
-                            <label>Username </label>
+                        <div className='text_feild'>
+                            <label >Username </label>
                             <span></span>
-                            <input
-                                type="text"
-                                name="username"
-                                value={values.username}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            ></input>
-                            {
-                                <p
-                                    style={{
-                                        fontSize: "10px",
-                                        marginBottom: "2px",
-                                        color: "#961d1d",
-                                    }}
-                                >
-                                    {errors.username}{" "}
-                                </p>
-                            }
+                            <input type="text" name='username' value={values.username} onChange={handleChange} onBlur={handleBlur}></input>
+                            {<p style={{ fontSize: '10px', marginBottom: '2px', color: '#961d1d' }}>{errors.username} </p>}
                         </div>
-                        <div className="text_feild">
-                            <label>Email</label>
+                        <div className='text_feild'>
+                            <label >Email</label>
                             <span></span>
-                            <input
-                                type="email"
-                                name="email"
-                                value={values.email}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            ></input>
-                            {
-                                <p
-                                    style={{
-                                        fontSize: "10px",
-                                        marginBottom: "2px",
-                                        color: "#961d1d",
-                                    }}
-                                >
-                                    {errors.email}
-                                </p>
-                            }
+                            <input type="email" name="email" value={values.email} onChange={handleChange} onBlur={handleBlur} ></input>
+                            {<p style={{ fontSize: '10px', marginBottom: '2px', color: '#961d1d' }}>{errors.email}</p>}
                         </div>
-                        <div className="text_feild">
+                        <div className='text_feild'>
                             <label>Password</label>
                             <span></span>
-                            <input
-                                type="password"
-                                name="password"
-                                value={values.password}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
-                            {
-                                <p
-                                    style={{
-                                        fontSize: "10px",
-                                        marginBottom: "2px",
-                                        color: "#961d1d",
-                                    }}
-                                >
-                                    {errors.password}{" "}
-                                </p>
-                            }
+                            <input type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+                            {<p style={{ fontSize: '10px', marginBottom: '2px', color: '#961d1d' }}>{errors.password} </p>}
                         </div>
-                        <div className="text_feild">
+                        <div className='text_feild'>
                             <label>Phone Number</label>
                             <span></span>
-                            <input
-                                type="tel"
-                                name="phone"
-                                value={values.phone}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
-                            {
-                                <p
-                                    style={{
-                                        fontSize: "10px",
-                                        marginBottom: "2px",
-                                        color: "#961d1d",
-                                    }}
-                                >
-                                    {errors.phone}
-                                </p>
-                            }
+                            <input type="tel" name="phone" value={values.phone} onChange={handleChange} onBlur={handleBlur} />
+                            {<p style={{ fontSize: '10px', marginBottom: '2px', color: '#961d1d' }}>{errors.phone}</p>}
                         </div>
-                        <div className="radio-btn">
-                            <div className="head">Gender</div>
-                            <div className="radio-flex">
-                                <input
-                                    type="radio"
-                                    id="male"
-                                    name="gender"
-                                    value="male"
-                                    onClick={clickhandler}
-                                ></input>{" "}
-                                <label htmlFor="male">Male</label>
-                                <input
-                                    type="radio"
-                                    id="female"
-                                    name="gender"
-                                    value="female"
-                                    onClick={clickhandler}
-                                ></input>{" "}
-                                <label htmlFor="female">Female</label>
-                                <input
-                                    type="radio"
-                                    id="others"
-                                    name="gender"
-                                    value="others"
-                                    onClick={clickhandler}
-                                ></input>{" "}
-                                <label htmlFor="others">Others</label>
+                        <div className='radio-btn' >
+                            <div className='head'>Gender</div>
+                            <div className='radio-flex'>
+                                <input type='radio' id='male' name='gender' value='male' onClick={clickhandler} ></input> <label htmlFor='male'>Male</label>
+                                <input type='radio' id='female' name='gender' value="female" onClick={clickhandler} ></input> <label htmlFor='female' >Female</label>
+                                <input type='radio' id='others' name='gender' value='others' onClick={clickhandler} ></input> <label htmlFor='others'>Others</label>
                             </div>
                         </div>
-                        <button
-                            className="lo-sub"
-                            style={{ marginTop: "10px", marginBottom: "10px" }}
-                            type="submit"
-                        >
-                            Registration
-                        </button>
+                        <button className="lo-sub" style={{ marginTop: "10px", marginBottom: '10px' }} type='submit'>Registration</button>
+
                     </form>
+
                 </div>
-                <p style={{ color: "white", marginTop: "5px" }}>
-                    Already have an account?<Link to={"/login"}>Sign In</Link>
-                </p>
+                <p style={{ color: "white", marginTop: '5px' }}>Already have an account?<Link to={'/login'}>Sign In</Link></p>
             </div>
+
+
         </div>
-    );
-};
+    )
+}
