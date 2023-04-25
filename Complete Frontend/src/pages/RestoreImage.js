0cbebb8aedbadf869220e7be8699ffd5d5b9657f
Sujan Maharjan <87776506+sujanW0W@users.noreply.github.com>
2023-04-25 22:52:52 +0545

    commitToPull.

--- Complete Frontend/src/pages/RestoreImage.js
+++ Complete Frontend/src/pages/RestoreImage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { PathContext } from "../components/UserContext";
 import "./RestoreImage.css";
 import axios from "axios";
